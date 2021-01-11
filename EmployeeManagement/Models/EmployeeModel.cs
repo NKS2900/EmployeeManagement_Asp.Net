6cdfde42f63129951fb5da754ddd378a53f8b45c
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-11 22:59:21 +0530

    [NIJAM] Add:Validation using JavaScript.

---
+++ EmployeeManagement/Models/EmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+   
+        
+    }
+}
--- EmployeeManagement/wwwroot/js/loginvalid.js
+++ EmployeeManagement/wwwroot/js/loginvalid.js
@@ -1,13 +1,20 @@
-﻿var email = document.forms['loginform']['email'];
-var pass = document.forms['loginform']['pass'];
 
-var PasswordPattern = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*.-])[a-zA-Z0-9].{8,}$/;
 
-function validated() {
-    if (PasswordPattern.test(email)) {
-        console.log("reeeeeee");
-        email.style.border = "1px solid red";
-        email.focus();
+var passreg = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*.-])[a-zA-Z0-9].{8,}$/;
+var emailreg =/^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\\@)[a-zA-Z0-9]+(\\.)[a-z]{2,4}$/;
+
+function validate() {
+
+    var email = document.getElementById(email).value;
+    var pass = document.getElementById(pass).value;
+    alert(email);
+    if (emailreg.test(email) && passreg.test(pass)) {
+        alert(email);
+        
+        return true;
+    }
+    else {
+        alert("invalid input");
         return false;
     }
-}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
