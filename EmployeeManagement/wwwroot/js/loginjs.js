58413c754ca74c4c0530c0f0bfbec371b6efb8f2
NIJMA SAYYAD <nksayyad2900@outlook.com>
2021-01-11 09:34:34 +0530

    [NIJAM] Add:Employee Management System Using Asp.Net

---
+++ EmployeeManagement/wwwroot/js/loginjs.js
@@ -0,0 +1,13 @@
+﻿var email = document.forms['loginform']['email'];
+var pass = document.forms['loginform']['pass'];
+
+var PasswordPattern = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*.-])[a-zA-Z0-9].{8,}$/;
+
+function validated() {
+    if (PasswordPattern.test(email)) {
+        console.log("reeeeeee");
+        email.style.border = "1px solid red";
+        email.focus();
+        return false;
+    }
+}
